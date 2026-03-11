ab67bf8d9cfb125f7aa160e945292e750dd379b6
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-11 16:57:05 +0100

    done

--- template/src/components/Expert.jsx
+++ template/src/components/Expert.jsx
@@ -2,7 +2,7 @@
 
 const Expert = () => {
   return (
-    <div>
+    <div className="mb-3">
          <button className='border-2 border-gray-400 bg-green-800 font-semibold text-sm p-2 rounded-sm text-white'>TALK TO AN EXPERT</button>
     </div>
   )
--- template/src/components/Testimonials.jsx
+++ template/src/components/Testimonials.jsx
@@ -1,9 +1,31 @@
-
+import Amazon from "../assets/Amazon.png";
+import Dell from "../assets/Dell.png";
+import southwest from "../assets/southwest.png";
+import harvard from "../assets/harvard.png";
+import Lendingtree from "../assets/Lendingtree.png"
 
 const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
+    const slide = [southwest, Amazon, harvard, Dell, Lendingtree, Amazon];
+    return (
+        <div className="flex flex-col overflow-hidden w-full bg-white py-6">
+            <div className="text-center py-10">
+                 <h2 className="font-bold text-sm">OVER 5,500 ORGANIZATION TRUST FORMASSEMBLY WITH THEIR DATA</h2>
+            </div>
+            <div className="relative">
+                <div className="flex slide-track gap-10">
+                    {slide.concat(slide).map((logo, i) => (
+                        <div key={i} className="flex shrink-0 w-40 items-center justify-center">
+                            <img className="w-50 h-10" src={logo} alt="" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="text-center py-15">
+                <p className="text-xl">No data collection strategy</p>
+                <p className="py-5 font-bold text-xl text-blue-950">You're losing more than you think</p>
+            </div>
+        </div>
+    )
 }
 
 export default Testimonials
